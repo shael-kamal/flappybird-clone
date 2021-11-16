5acc059bfbfd2eec00a48c1925c57862b56f8d25
Shael Kamal <shael.kamal@gmail.com>
2021-11-16 15:04:59 +0600

    Added pipe spawns
    Pipes will now appear at random heights, mechanics still need to be added

--- main.py
+++ main.py
@@ -6,18 +6,34 @@ Assets: https://github.com/samuelcust/flappy-bird-assets
 Audio: https://www.sounds-resource.com/mobile/flappybird/sound/5309/
 """
 
-import pygame, sys
+import pygame, sys, random
 
-
+# Functions
 def draw_floor():
     screen.blit(floor_surface, (floor_x_position, 400))
     screen.blit(floor_surface, (floor_x_position + s_dimensions[0], 400))
 
+def create_pipe():
+    random_pipe_pos = random.choice(pipe_height)
+    bottom_pipe = pipe_surface.get_rect(midtop = (350,random_pipe_pos))  #creates new pipes
+    top_pipe = pipe_surface.get_rect(midbottom = (350,random_pipe_pos-100))
+    return bottom_pipe,top_pipe
 
+def move_pipes(pipes):  #takes list of pipes as an argument
+    for pipe in pipes:
+        pipe.centerx -= 2  #Changes center x coordinated of each pipe in list
+    return pipes
+
+def draw_pipes(pipes): #takes list of pipes as arg
+    for pipe in pipes:
+        if pipe.bottom>512:
+            screen.blit(pipe_surface,pipe)
+        else:
+            screen.blit(pygame.transform.rotate(pipe_surface,180), pipe)
 pygame.init()  # Initializes pyGame
 
 # Create a surface to display game, normally called a screen, arguments are the dimensions in a tuple (width,height).
-screen = pygame.display.set_mode((288, 576))
+screen = pygame.display.set_mode((288, 512))
 
 # time: a pygame module that monitors time
 clock = pygame.time.Clock()
@@ -41,6 +57,12 @@ bird_surface = pygame.image.load('assets/sprites/bluebird-midflap.png').convert(
 # bird_surface = pygame.transform.scale2x(bird_surface)
 bird_rect = bird_surface.get_rect(center=(50, 256))  # rect: will be used for collisions
 
+pipe_surface = pygame.image.load('assets/sprites/pipe-green.png').convert()  #pipe image
+pipe_list = []  #will be addind rectangles to the list
+SPAWNPIPE = pygame.USEREVENT  # new event that will be used as a timer
+pygame.time.set_timer(SPAWNPIPE,1200)  #event that will be triggered every 1.2 seconds
+pipe_height = [200,250,300,350,400]  # list of possible heights of pipes
+
 # Start the game loop
 while True:  # This runs until loop is broken from the inside
     # Create event loop to listen for events every iteration of the while loop
@@ -54,13 +76,21 @@ while True:  # This runs until loop is broken from the inside
                 bird_movement = 0  # Nullifies effect of gravity
                 bird_movement -= 6  # Modifies bird movement
 
+        if event.type == SPAWNPIPE:  # Creating a listener to spawn pipes at every interval
+            pipe_list.extend(create_pipe())
     # Put images on the screen, Must be inside game loop. surface1.blit(surface2,(x,y))
     screen.blit(bg_surface, (0, 0))  # blit: method used to put one surface on another one.
 
+    #BIRD
     bird_movement += gravity
     bird_rect.centery += bird_movement  # changes y axis of center by bird_movement variable
     screen.blit(bird_surface, bird_rect)  # draws bird on the main surface
 
+    #PIPE
+    move_pipes(pipe_list)
+    draw_pipes(pipe_list)
+
+    #FLOOR
     floor_x_position -= 1  # X position of floor_surface is updated every iteration (creates moving floor)
     draw_floor()  # Function for drawing floor
     if floor_x_position <= -288:  # Conditional to create endless floor
