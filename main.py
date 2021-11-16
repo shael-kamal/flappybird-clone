2df98abc8e05ba990b4ff49d4ce9b0c03797f0b2
Shael Kamal <shael.kamal@gmail.com>
2021-11-16 12:16:41 +0600

    Background image
    Imported, added and scaled background image

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ pygame.init()  # Initializes pyGame
 # Variables
 frames = 120  # How many frames refreshed per second
 s_dimensions = (576, 1024)  # Tuple with dimensions of the screen variable (width, height)
+# Images - Imported images are surfaces stored inside variables (similar to display surface - screen)
+bg_surface = pygame.image.load('assets/sprites/background-day.png').convert()  # background image
+bg_surface = pygame.transform.scale2x(bg_surface)  # transform: module that lets you do geometric transformations
 
 # Create a surface to display game, normally called a screen, arguments are the dimensions in a tuple (width,height).
 screen = pygame.display.set_mode(s_dimensions)
@@ -29,6 +32,8 @@ while True:  # This runs until loop is broken from the inside
         if event.type == pygame.QUIT:  # Checks type of event
             pygame.quit()  # This is the opposite of pygame.init()
             sys.exit()  # Allows us to completely exit window. Uses sys module
+    # Put images on the screen, Must be inside game loop. surface1.blit(surface2,(x,y))
+    screen.blit(bg_surface, (0, 0))  # blit: method used to put one surface on another one.
 
     pygame.display.update()  # Refresh everything above it while the loop runs
     clock.tick(frames)  # Sets target FPS to the one specified (can run slower but will not run faster).
